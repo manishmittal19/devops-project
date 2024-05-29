01dbf554487e211756cbd9ce69207e4396077149
Manish Mittal <manishmittal19@yahoo.com>
2024-05-29 16:27:35 -0700

    letsgo docker

---
+++ apps/letsgo/go.mod
@@ -0,0 +1,3 @@
+module github.com/manishm1907/devops-project/apps/letsgo
+
+go 1.21.3
---
+++ apps/letsgo/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, string("let's go app!"))
+}
+
+func readiness(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, string("i'm ready!!"))
+}
+
+func liveness(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, string("i'm alive!!"))
+}
+
+func appinfo(w http.ResponseWriter, r *http.Request) {
+
+	deployType := os.Getenv("DEPLOYMENT_TYPE")
+	if deployType == "" {
+		deployType = "UNKNOWN"
+	}
+
+	appInfo := map[string]string{
+		"appname":         "letsgo",
+		"deployment_type": deployType,
+	}
+
+	if appInfoJSON, err := json.Marshal(appInfo); err != nil {
+		http.Error(w, "Internal Server Error", 500)
+	} else {
+		fmt.Fprintf(w, string(appInfoJSON))
+	}
+
+	return
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/ready", readiness)
+	mux.HandleFunc("/live", liveness)
+	mux.HandleFunc("/appinfo", appinfo)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
